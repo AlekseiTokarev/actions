b8b9fc72040250b5a2d6c1716bf677e72660dafe
Aleksei Tokarev <atokarev@hylamobile.com>
2020-05-06 11:07:13 -0500

    add 2 tests

---
+++ src/test/kotlin/com/hylamobile/ActionAppTest.kt
@@ -0,0 +1,20 @@
+package com.hylamobile
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+
+object ActionAppTest {
+
+    @Test
+    fun `Adding 1 and 3 should be equal to 4`() {
+        Assertions.assertEquals(1 + 3, 4)
+    }
+
+    @Test
+    fun `Adding 1 and 2 should be equal to 3`() {
+        Assertions.assertEquals(1 + 2, 5)
+    }
+}
+
+
